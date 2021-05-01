fafe496e1acfd0e234d3cdd708456e164bdb7936
FrenkenFlores <fflores@student.21-school.ru>
2021-05-01 03:32:52 +0300

    update #3: task_3

---
+++ findian/findian.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+func main() {
+	var s string
+	fmt.Println("Enter a string")
+	fmt.Sscan()
+	fmt.Scan()
+	fmt.Scanln(&s)
+	if strings.Compare(string(s[0]), "i") == 0 &&
+		strings.Contains(s, "a") == true &&
+		strings.Compare(string(s[len(s) - 1]), "n") == 0{
+		fmt.Println("Found!")
+	} else {
+		fmt.Println("Not Found!")
+	}
+}
